eb5d7835af946cb68610bffa2c8e64ce22c85c1c
Natalia Alvarez <169085366+natalia-alvarez-mck@users.noreply.github.com>
2024-10-03 16:27:42 -0400

    updating files

--- next-app/app/page.js
+++ next-app/app/page.js
@@ -2,5 +2,10 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <h1>TNP Arcade</h1>;
+  return (
+    <>
+      <h1>TNP Arcade</h1>
+      <p>Choose a game!</p>
+    </>
+  );
 }

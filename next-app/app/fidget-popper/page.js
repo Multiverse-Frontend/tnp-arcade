63084cca6bdf9d642130ebf2f388932708b1ade7
TatianaS7 <tstwrt7@gmail.com>
2024-10-04 14:04:14 -0400

    Added comments

--- next-app/app/fidget-popper/page.js
+++ next-app/app/fidget-popper/page.js
@@ -23,17 +23,21 @@ function SelectPopper() {
 
 
     function setPopper(popper) {
+        // Reset currentPopper state
         setCurrentPopper(null);
+        // Check if allPopperData is an array and not empty
         if (Array.isArray(allPopperData) && allPopperData.length > 0) {
             // Loop through allPopperData array to find the selected popper
             allPopperData && allPopperData.forEach((data, idx) => {
+                // Check if the selected popper exists in the data object
                 if (data.hasOwnProperty(popper)) {
+                    // Set currentPopper state to the selected popper
                     const selectedPopper = data[popper];
-                    console.log(selectedPopper); 
-                    console.log(selectedPopper.bubbleCount);
-                    console.log(selectedPopper.bubbleColor);
-                    console.log(selectedPopper.name); 
-                    console.log(selectedPopper.image);
+                    // console.log(selectedPopper); 
+                    // console.log(selectedPopper.bubbleCount);
+                    // console.log(selectedPopper.bubbleColor);
+                    // console.log(selectedPopper.name); 
+                    // console.log(selectedPopper.image);
                     setCurrentPopper(selectedPopper);
                 } else {
                     console.error("popperData is not an array or is empty");
@@ -52,6 +56,7 @@ function SelectPopper() {
                     {allPopperData && allPopperData.map((data, index) => {
                         // Map through each popper object in the array
                         return Object.keys(data).map((popper, idx) => {
+                            // Return a button for each popper object with corresponding style and image
                             return (
                                 <button key={idx} id={popper} value={popper} style={{border: `3px solid ${data[popper].theme} `}} onClick={() => setPopper(popper)}>
                                     <img src={data[popper].image} alt={data[popper].name} />
@@ -63,10 +68,14 @@ function SelectPopper() {
                 <div className="bubbles" style={{top: currentPopper ? `${currentPopper.top}` : "58%", left: currentPopper && `${currentPopper.left}` }}>
                     {/* Create bubbles for bubble count value from JSON */}
                     {currentPopper && currentPopper.rowPattern && (() => {
+                        // Access the rowPattern array from the currentPopper object
                         const rowPattern = currentPopper.rowPattern;
+                        // Initialize bubbleIndex to 0
                         let bubbleIndex = 0;
+                        // Map through the rowPattern array to create rows and bubbles
                         return rowPattern.map((count, rowIndex) => (
                             <div key={rowIndex} className="bubble-row">
+                                {/* Create bubbles for each row using the count value */}
                                 {Array.from({ length: count }).map((_, colIndex) => {
                                     return (
                                         <button key={bubbleIndex++} style={{ backgroundColor: currentPopper.bubbleColor, width: currentPopper.bubbleSize, height: currentPopper.bubbleSize }} className="bubble"></button>
